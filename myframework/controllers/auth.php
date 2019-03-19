7fdd773d7ab1cb002939c083cc0b8cbf76795155
Austin-Wise <austin@wisebydesign.io>
2019-03-18 20:10:33 -0400

    Assignment 6 requirements met, 'Login With Forms' through file reading and auth.php

--- myframework/controllers/auth.php
+++ myframework/controllers/auth.php
@@ -5,16 +5,27 @@ class auth extends AppController{
     }
 
     public function login(){
-        if(@$_REQUEST["username"] && @$_REQUEST["password"]){
-
-            if(@$_REQUEST["username"] == "bobby123" && @$_REQUEST["password"] == "Root"){
-                $_SESSION["loggedin"] = 1;
-                header("Location:/welcome");
-            }else{
-                header("Location:/welcome?msg=Bad Login");
-            } 
-        }else{
-            header("Location:/welcome?msg=Bad Login");
+        if ($_REQUEST["username"] && $_REQUEST["password"]) {
+			$path = "./assets/accounts.txt";
+			if ( file_exists($path) ) {
+				$users = file($path);
+				foreach ($users as $data) {
+					$user = explode("|", $data);
+					if ($_REQUEST["username"] == $user[0] && $_REQUEST["password"] == $user[1]) {
+						$_SESSION["loggedin"] = 1;
+						$_SESSION["username"] = $_REQUEST["username"];
+						$_SESSION["bio"] = $user[2];
+						header("Location:/welcome");
+					}
+				}
+				if ($_SESSION["loggedin"] != 1) {
+					header("Location:/welcome?msg=Bad Login");
+				}
+			} else {
+				header("Location:/welcome?msg=Server Error, Bad Login");
+			}
+		} else {
+			header("Location:/welcome?msg=Missing username or password");
         }
     }
     public function logout(){
@@ -22,5 +33,18 @@ class auth extends AppController{
         header("Location:/welcome");
     }
 }
+
+// if(@$_REQUEST["username"] && @$_REQUEST["password"]){
+
+//     if(@$_REQUEST["username"] == "bobby123" && @$_REQUEST["password"] == "Root"){
+//         $_SESSION["loggedin"] = 1;
+//         header("Location:/welcome");
+//     }else{
+//         header("Location:/welcome?msg=Bad Login");
+//     } 
+// }else{
+//     header("Location:/welcome?msg=Bad Login");
+// }
+// }
 
 ?>
